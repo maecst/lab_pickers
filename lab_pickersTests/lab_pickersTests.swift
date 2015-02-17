f8ad6be8a5f7c21042a0ca9ef771fe15869e2bd3
Mae Yee <mae@Alans-iMac.local>
2015-02-17 00:38:41 -0800

    Initial Commit

---
+++ lab_pickersTests/lab_pickersTests.swift
@@ -0,0 +1,36 @@
+//
+//  lab_pickersTests.swift
+//  lab_pickersTests
+//
+//  Created by Mae Yee on 2015-02-17.
+//  Copyright (c) 2015 Mae Yee. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class lab_pickersTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
